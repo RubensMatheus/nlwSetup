f031fac59c1de1f2ee4725482dc82f6bd182e5ef
“Rubens <rubensmatheusvenancio@gmail.com>
2023-01-22 21:29:52 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const form = document.querySelector("form")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
+
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add(){
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso ❌")
+    return
+  }
+
+  alert("Adicionado com sucesso ✅")
+  nlwSetup.addDay(today)
+}
+
+function save(){
+  localStorage.setItem("setup", JSON.stringify(nlwSetup.data))
+}
+
+
+// const data = {
+//   run: ["01-19", "01-20", "01-21"],
+//   water: ["01-20"],
+// }
+
+const data = JSON.parse(localStorage.getItem("setup")) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
